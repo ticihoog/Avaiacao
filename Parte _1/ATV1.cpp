c2b065850b1c7843278e44ed2c840da857bae1fb
ticihoog <ticianapaternostro@gmail.com>
2023-09-18 17:11:57 -0300

    final aula 1 - atv01

---
+++ Parte _1/ATV1.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm> 
+
+using namespace std;
+
+
+struct Cliente {
+    string cpf;
+    string nome;
+    string dataNascimento;
+    string cnh;
+};
+
+
+struct Veiculo {
+    string renavan;
+    string placa;
+    string dataHoraRetirada;
+    string dataHoraEntrega;
+    string lojaRetirada;
+};
+
+
+void incluirCliente(vector<Cliente> &clientes) {
+    Cliente novoCliente;
+    cout << "CPF: ";
+    cin >> novoCliente.cpf;
+    cin.ignore(); 
+    cout << "Nome: ";
+    getline(cin, novoCliente.nome);
+    cout << "Data de Nascimento: ";
+    getline(cin, novoCliente.dataNascimento);
+    cout << "Número da CNH: ";
+    getline(cin, novoCliente.cnh);
+
+    clientes.push_back(novoCliente);
+    cout << "Cliente adicionado com sucesso!" << endl;
+}
+
+
+void excluirCliente(vector<Cliente> &clientes, const string &cpf) {
+    auto it = remove_if(clientes.begin(), clientes.end(),
+                        [&](const Cliente &c) { return c.cpf == cpf; });
+
+    if (it != clientes.end()) {
+        clientes.erase(it, clientes.end());
+        cout << "Cliente excluído com sucesso!" << endl;
+    } else {
+        cout << "Cliente não encontrado." << endl;
+    }
+}
+
+
+void listarClientes(const vector<Cliente> &clientes) {
+    if (clientes.empty()) {
+        cout << "Nenhum cliente cadastrado ainda." << endl;
+    } else {
+        cout << "\nLista de Clientes:" << endl;
+        for (const Cliente &cliente : clientes) {
+            cout << "CPF: " << cliente.cpf << endl;
+            cout << "Nome: " << cliente.nome << endl;
+            cout << "Data de Nascimento: " << cliente.dataNascimento << endl;
+            cout << "CNH: " << cliente.cnh << endl;
+            cout << "------------------------" << endl;
+        }
+    }
+}
+
+
+void localizarCliente(const vector<Cliente> &clientes, const string &cpf) {
+    bool encontrado = false;
+    for (const Cliente &cliente : clientes) {
+        if (cliente.cpf == cpf) {
+            cout << "CPF: " << cliente.cpf << endl;
+            cout << "Nome: " << cliente.nome << endl;
+            cout << "Data de Nascimento: " << cliente.dataNascimento << endl;
+            cout << "CNH: " << cliente.cnh << endl;
+            encontrado = true;
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        cout << "Cliente não encontrado." << endl;
+    }
+}
+
+
+void incluirVeiculo(vector<Veiculo> &veiculos) {
+    Veiculo novoVeiculo;
+    cout << "Renavan: ";
+    cin >> novoVeiculo.renavan;
+    cin.ignore();
+    cout << "Placa: ";
+    getline(cin, novoVeiculo.placa);
+    cout << "Data e Hora de Retirada (prevista): ";
+    getline(cin, novoVeiculo.dataHoraRetirada);
+    cout << "Data e Hora de Entrega (prevista): ";
+    getline(cin, novoVeiculo.dataHoraEntrega);
+    cout << "Loja de Retirada: ";
+    getline(cin, novoVeiculo.lojaRetirada);
+
+    veiculos.push_back(novoVeiculo);
+    cout << "Veículo adicionado com sucesso!" << endl;
+}
+
+
+void excluirVeiculo(vector<Veiculo> &veiculos, const string &placa) {
+    auto it = remove_if(veiculos.begin(), veiculos.end(),
+                        [&](const Veiculo &v) { return v.placa == placa; });
+
+    if (it != veiculos.end()) {
+        veiculos.erase(it, veiculos.end());
+        cout << "Veículo excluído com sucesso!" << endl;
+    } else {
+        cout << "Veículo não encontrado." << endl;
+    }
+}
+
+
+void listarVeiculos(const vector<Veiculo> &veiculos) {
+    if (veiculos.empty()) {
+        cout << "Nenhum veículo cadastrado ainda." << endl;
+    } else {
+        cout << "\nLista de Veículos:" << endl;
+        for (const Veiculo &veiculo : veiculos) {
+            cout << "Renavan: " << veiculo.renavan << endl;
+            cout << "Placa: " << veiculo.placa << endl;
+            cout << "Data e Hora de Retirada (prevista): " << veiculo.dataHoraRetirada << endl;
+            cout << "Data e Hora de Entrega (prevista): " << veiculo.dataHoraEntrega << endl;
+            cout << "Loja de Retirada: " << veiculo.lojaRetirada << endl;
+            cout << "------------------------" << endl;
+        }
+    }
+}
+
+
+void localizarVeiculo(const vector<Veiculo> &veiculos, const string &placa) {
+    bool encontrado = false;
+    for (const Veiculo &veiculo : veiculos) {
+        if (veiculo.placa == placa) {
+            cout << "Renavan: " << veiculo.renavan << endl;
+            cout << "Placa: " << veiculo.placa << endl;
+            cout << "Data e Hora de Retirada (prevista): " << veiculo.dataHoraRetirada << endl;
+            cout << "Data e Hora de Entrega (prevista): " << veiculo.dataHoraEntrega << endl;
+            cout << "Loja de Retirada: " << veiculo.lojaRetirada << endl;
+            encontrado = true;
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        cout << "Veículo não encontrado." << endl;
+    }
+}
+
+
+
+    return 0;
+}
